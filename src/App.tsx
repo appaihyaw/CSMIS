b307b2a336e52838201c6ff37d65cf1965319db2
Kdrighteo <kdrighteo@gmail.com>
2023-06-23 19:52:59 +0000

    Created a Login componenet and rendered it in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import Login from './components/Login';
 
 function App() {
   return (
-    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0 bg-white">
       <div className="md:w-1/3 max-w-sm">
         <img
           src="University_of_Ghana.png" alt="ugLogo" />
       </div>
-      <div className="md:w-1/3 max-w-sm">
-        <div className="text-center md:text-left">
-          <Dropdown />
+      <div className=" w-[500px] bg-gray-100 h-60 p-10 flex justify-start rounded shadow-md"> 
+        <div>
+          <div className=" mb-5">
+            <Dropdown />
+          </div>
+          <div>
+             <Login/>
+          </div>
+         
         </div>
       </div>
-      <div>
-          <Login/>
-      </div>
+       
     </section>
 
   );
