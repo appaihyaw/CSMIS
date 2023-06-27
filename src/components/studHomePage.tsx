7e24e55dfe0ace9aa98c90538164de6f03cb0f48
iTornam <einsteinernest@gmail.com>
2023-06-27 13:10:29 +0000

    Added "studHomePage" file along with new dependencies
    install react-router-dom for page navigations
    install react-icons dependencies.
    install hero-icon dependencies.
    installed "prettier" extension for vs code formatting.
    The code in studHomePage can be ran after we render the homepage from the App.tsx file

---
+++ src/components/studHomePage.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";  
+import { Link } from "react-router-dom";
+import {HiMenu} from "react-icons/hi"
+import {AiOutlineHome} from "react-icons/ai"
+import {PiStudentFill} from "react-icons/pi"
+import {SiGoogleclassroom} from "react-icons/si"
+import {LiaUserEditSolid} from "react-icons/lia"
+import {ImStatsDots} from "react-icons/im"
+import {IoMdSettings} from "react-icons/io"
+import {GrResources} from "react-icons/gr"
+import {BiLogOut} from "react-icons/bi"
+
+
+
+
+const studHome = () => {
+    // Function body
+    const menus = [
+      {name:"Home",link:'/',icon: AiOutlineHome},
+      {name:"Students",link:'/',icon: PiStudentFill},
+      {name:"Classes",link:'/',icon: SiGoogleclassroom},
+      {name:"Records",link:'/',icon: LiaUserEditSolid},
+      {name:"Satistics",link:'/',icon: ImStatsDots},
+      {name:"Settings",link:'/',icon: IoMdSettings},
+      {name:"Resources",link:'/',icon: GrResources},
+      {name:"Sign Out",link:'/',icon: BiLogOut},
+
+    ] 
+    const [open,setOpen] = useState(true);
+    return (
+      // JSX/tsx code
+      <section className="flex gap-6">   
+        <div className={'bg-[#18345C] min-h-screen ${open ? w-72:w-16 } duration-500 text-gray-100 px-4 '}>
+          <div>
+            <HiMenu size={25} className="cursor-pointer" onClick={() => setOpen(!open)}/>
+          </div>
+          <div>
+            {
+              menus?.map((menu,i)=>(
+            <Link 
+              to={menu?.link}
+              key={i}
+              className={'group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-slate-300 round'}
+              >
+              <div> {React.createElement(menu?.icon, {size:'20'})}</div>
+              <h2 style={{transitionDelay: '${i+3}00msl'}} className={'whitespace-pre duration-500 ${ !open && "opacity-0 translate-x-28 overflow-hidden"  }' }> {menu?.name} </h2>
+              <h2 className={'${absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-lg drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit' }>
+                {menu?.name}
+              </h2>
+            </Link>
+              ))
+}
+              ))
+            
+          </div>
+        
+        </div>
+        
+      </section>
+
+    );
+  };
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        "dark-blue":"#18345C"
+        
+      }
+    },
   },
   plugins: [],
 }
