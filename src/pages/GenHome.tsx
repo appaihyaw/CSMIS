95bc76044208bad74160e7c4ef0a07ca689fee8a
Gilbert Danso <72208611+kdrighteo@users.noreply.github.com>
2023-07-07 17:36:38 +0000

    hover effects

--- src/pages/GenHome.tsx
+++ src/pages/GenHome.tsx
@@ -8,44 +8,36 @@ export default function GenHome() {
       <div className="">
         <main>
           <Outlet />
-          <div className=" flex bg-indigo-800 text-white">
-            <div className=" flex-1">
-              <ul>
-                <li></li>
-              </ul>
-            </div>
-            <div className=" flex-1">
+          <div className=" flex bg-indigo-800 text-white pl-20">
+
+            <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>Academics</li>
               </ul>
             </div>
-            <div className=" flex-1">
-              <ul>
-                <li></li>
-              </ul>
-            </div>
-            <div className=" flex-1">
+            
+            <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>Research</li>
               </ul>
             </div>
-            <div className=" flex-1">
+            <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>Library</li>
               </ul>
             </div>
-            <div className=" flex-1">
+            <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>International Programmes</li>
               </ul>
             </div>
-            <div className=" flex-1">
+            <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900 pl-20">
               <ul>
                 <li>Freshmen info</li>
               </ul>
             </div>
           </div>
-          <div className=" flex justify-center">
+          <div className=" flex justify-cente ">
             <img src=" ug main2.jpeg" alt="" width={2000} height="100" />
           </div>
         </main>
