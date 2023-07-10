a56091490ad63f2a37e492dc42dd651135e06b20
iTornam <einsteinernest@gmail.com>
2023-07-10 23:18:02 +0000

    ROUTING AND STYLING
    General Homepage is now accessible via sign out from staff homepage.
    NavBar no more rendered on  staffHomePage as per UI.
    Only 2 links (Home=> General Home page AND Login=> Log page) will be accessible from the General homepage. For now, staff homepage is there to help easy access to view pages while they are styled.

--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -5,15 +5,9 @@ function RootLayout() {
   return (
     <div className="root-layout">
       <header>
-         <nav className="container pt-2 mx-auto">
+        <nav className="container pt-2 mx-auto">
           <div className=" flex justify-between">
-            <div className=" order-1">
-              <svg viewBox="0 0 100 80" width="20" height="20" className=" to-blue-50">
-                <rect width="100" height="20"></rect>
-                <rect y="30" width="100" height="20"></rect>
-                <rect y="60" width="100" height="20"></rect>
-              </svg>
-            </div>
+            <div className=" order-1"></div>
             <div className=" order-2 space-x-4 ">
               <NavLink to="GenHome">Home</NavLink>
               <NavLink to="Log">Login</NavLink>
@@ -31,7 +25,6 @@ function RootLayout() {
             alt="uglogo"
           />
         </div>
-       
       </header>
       <main>
         <Outlet />
--- src/pages/GenHome.tsx
+++ src/pages/GenHome.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import Log from "./Log";
+import StaffHome from "./StaffHomePage";
 
 export default function GenHome() {
   return (
     <div>
-      <div className="">                                                                                                
-        <main>
-          <Outlet />
-          <div className=" flex bg-indigo-800 text-white pl-20">
+      <header>
+        <nav className="container pt-2 mx-auto">
+          <div className=" flex justify-between">
+            <div className=" order-1"></div>
+            <div className=" order-2 space-x-4 ">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="Log">Login</NavLink>
+              <NavLink to="StaffHomePage">Staff Home</NavLink>
+            </div>
+          </div>
+        </nav>
 
+        <div className=" flex justify-center pl-5 pb-4 text-2xl font-medium text-blue-500 space-x-1">
+          <h3>csMIS</h3>
+          <img
+            src=" https://www.ug.edu.gh/sites/default/files/ug_standard_new_sl_1.png"
+            width="80"
+            height="60"
+            alt="uglogo"
+          />
+        </div>
+      </header>
+      <div className="">
+        <main>
+          <div className=" flex bg-indigo-800 text-white pl-20">
             <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>Academics</li>
               </ul>
             </div>
-            
+
             <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>Research</li>
@@ -40,7 +61,6 @@ export default function GenHome() {
           <div className=" flex justify-cente ">
             <img src=" ug main2.jpeg" alt="" width={2000} height="100" />
           </div>
-          
         </main>
       </div>
     </div>
