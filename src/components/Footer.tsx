b5c5af57d5a2ac7ae56c5fe6dc55e7f2d116ec8b
kdrighteo <72208611+kdrighteo@users.noreply.github.com>
2023-07-20 19:12:30 +0000

    Still Styling GenHome

---
+++ src/components/Footer.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div className=" bg-blue-950 grid grid-cols-5 gap-1 text-center h-40 shadow-md m-6 mb-0 ">
+      
+            <div className=" grid grid-cols-1 rounded-sm underline">
+                <h1 className=" uppercase underline">Contact Us</h1>
+                <p>Email: kdrighteo@gmail.com</p>
+                <p>Tel: +233558644950</p>
+                <p>Loc: Accra, Ghana</p>
+            </div>
+            <div className=" grid grid-cols-1 rounded-sm underline">
+                <h1 className=" uppercase underline">Contact Us</h1>
+                <p>Email: kdrighteo@gmail.com</p>
+                <p>Tel: +233558644950</p>
+                <p>Loc: Accra, Ghana</p>
+            </div>
+            <div className=" grid grid-cols-1 rounded-sm underline">
+                <h1 className=" uppercase underline">Contact Us</h1>
+                <p>Email: kdrighteo@gmail.com</p>
+                <p>Tel: +233558644950</p>
+                <p>Loc: Accra, Ghana</p>
+            </div>
+            <div className=" grid grid-cols-1 rounded-sm underline">
+                <h1 className=" uppercase underline">Contact Us</h1>
+                <p>Email: kdrighteo@gmail.com</p>
+                <p>Tel: +233558644950</p>
+                <p>Loc: Accra, Ghana</p>
+            </div>
+            <div className=" grid grid-cols-1 rounded-sm underline">
+                <h1 className=" uppercase underline">Contact Us</h1>
+                <p>Email: kdrighteo@gmail.com</p>
+                <p>Tel: +233558644950</p>
+                <p>Loc: Accra, Ghana</p>
+            </div>
+        </div>
+        <div className=" bg-amber-950 mt-0 m-6 h-8 text-yellow-100 flex justify-between text-sm text-center">
+            <div id="powered"> Copyright ©&nbsp;2023,&nbsp;<a href="https://www.ug.edu.gh/">University of Ghana</a></div>
+            <div className="footer_btm_inner"> <a href="https://twitter.com/UnivofGh" target="_blank" className="icon_tweet" title="Twitter">Twitter</a> <a href="https://plus.google.com/u/0/+universityofghana/posts" target="_blank" className="icon_google" title="Google+">Google+</a> <a href="https://www.facebook.com/univofgh" target="_blank" className="icon_facebook" title="Facebook">Facebook</a> <a href="http://www.youtube.com/user/universityghana" className="icon_skype" title="youtube" target="_blank">youtube</a></div>
+        </div>
+    </div>
+   
+  );
+}
+
+export default Footer
--- src/pages/GenHome.tsx
+++ src/pages/GenHome.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Log from "./Log";
 import StaffHome from "./staff Pages/StaffHomePage";
+import Footer from "../components/Footer";
 
 export default function GenHome() {
   return (
     <div>
       <header>
-        <nav className="container pt-2 mx-auto">
-          <div className=" flex justify-between">
+        <nav className=" m-6 mt-0 mb-0 bg-blue-950 text-white">
+          <div className=" flex justify-between ">
             <div className=" order-1"></div>
-            <div className=" order-2 space-x-4 ">
+            <div className=" order-2 space-x-4">
               <NavLink to="/">Home</NavLink>
               <NavLink to="Log">Login</NavLink>
               <NavLink to="StaffHomePage">Staff Homepage</NavLink>
@@ -19,7 +20,7 @@ export default function GenHome() {
           </div>
         </nav>
 
-        <div className=" flex justify-center pl-5 pb-4 text-2xl font-medium text-blue-500 space-x-1">
+        <div className=" flex justify-center m-6 text-2xl font-medium text-blue-500 space-x-1">
           <h3>csMIS</h3>
           <img
             src=" https://www.ug.edu.gh/sites/default/files/ug_standard_new_sl_1.png"
@@ -31,7 +32,7 @@ export default function GenHome() {
       </header>
       <div className="">
         <main>
-          <div className=" flex bg-indigo-800 text-white pl-20">
+          <div className=" flex bg-blue-950 text-white pl-20 m-6 mt-0">
             <div className=" flex-1 hover:bg-blue-300 hover:text-gray-900">
               <ul>
                 <li>Academics</li>
@@ -59,11 +60,30 @@ export default function GenHome() {
               </ul>
             </div>
           </div>
-          <div className=" flex justify-cente ">
-            <img src=" ug main2.jpeg" alt="" width={2000} height={100} />
+          <div className=" flex justify-center h-72 m-6">
+            <img src=" ug main2.jpeg" alt="" width={2000}/>
+          </div>
+          <div className=" flex justify-between m-6 mb-0">
+            <div className=" text-green-600">
+              <h4>News</h4>
+            </div>
+            <div className="">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="indigo" className="bi bi-arrow-left-circle" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/> </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="indigo" className="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16"> <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/> </svg>
+            </div>
+          </div>
+          <div className=" grid grid-cols-6 gap-1 text-center h-40 shadow-md m-6 mt-0">
+            <div className=" grid grid-cols-1 bg-slate-400 rounded-sm underline"></div>
+            <div className=" grid grid-cols-1 bg-slate-400 rounded-sm underline"></div>
+            <div className=" grid grid-cols-1 bg-slate-400 rounded-sm underline"></div>
+            <div className=" grid grid-cols-1 bg-slate-400 rounded-sm underline"></div>
+            <div className=" grid grid-cols-1 bg-slate-400 rounded-sm underline"></div>
+            <div className=" grid grid-cols-1 bg-slate-400 rounded-sm underline"></div>
           </div>
         </main>
+
       </div>
+      < Footer/>
     </div>
   );
 }
