328b83fd00798fa7af732c3329b06b03c8f501ac
kdrighteo <72208611+kdrighteo@users.noreply.github.com>
2023-07-21 16:43:53 +0000

    ....

--- src/pages/GenHome.tsx
+++ src/pages/GenHome.tsx
@@ -72,7 +72,7 @@ export default function GenHome() {
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="indigo" className="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16"> <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/> </svg>
             </div>
           </div>
-          <div className=" grid grid-cols-5 gap-3 text-center h-48 shadow-md m-6 mt-0 text-xs text-blue-400">
+          <div className=" grid grid-cols-5 gap-2 text-center h-48 shadow-md m-6 mt-0 text-xs text-blue-400">
             <div className=" grid grid-cols-1 bg-white rounded-sm underline">
               <img src="batch.jpg"/>
               <h4 className=" "><a href="/news/department-theatre-arts-receives-audio-visual-equipment-johnson-children%E2%80%99s-school-foundation">Department of Theatre Arts Receives Audio-Visual...</a></h4>
@@ -113,8 +113,11 @@ export default function GenHome() {
               </ul>
             </div>
 
-            <div className=" grid grid-cols-1 bg-white rounded-sm underline">
-
+            <div className=" grid grid-cols-1 bg-black rounded-sm underline">
+              <h1 className=" text-xl">Ads</h1>
+              <h1 className=" text-xl">Ads</h1>
+              <h1 className=" text-xl">Ads</h1>
+              <h1 className=" text-xl">Ads</h1>
             </div>
 
           </div>
